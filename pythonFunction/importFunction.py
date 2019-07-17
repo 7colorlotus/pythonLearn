20b3b9b665f1b88e66b5e14f0ca22a1aaa47666c
JasonZhu <jason.zhu@d1m.cn>
2019-07-17 17:02:48 +0800

    format code

--- pythonFunction/importFunction.py
+++ pythonFunction/importFunction.py
@@ -1,5 +1,11 @@
 import math
-def move(x,y,step,angle=0):
+
+
+def move(x, y, step, angle=0):
 	nx = x + step * math.cos(angle)
 	ny = y - step * math.sin(angle)
-	return nx,ny
+	return nx, ny
+
+
+print(move(10, 20, 100))
+print(math.sin(0))
